3848a6fe6b6c264b946ccfa68741224369836a00
yaza <wayan13yasa@gmail.com>
2021-03-26 14:40:36 +0800

    fixing base url call

--- core/Request.php
+++ core/Request.php
@@ -2,11 +2,24 @@
 
 namespace Core;
 
+use Illuminate\Support\Facades\URL;
+
 class Request
 {
     public function getPath()
     {
         $path = $_SERVER['REQUEST_URI'] ?? '/';
+
+        // cek position public path
+        $public = strpos($path, 'public/');
+
+        // get path after public/
+        if($public)
+        {
+            $path = substr($path, $public+6);
+        }
+
+        // cek ? param
         $position = strpos($path, '?');
 
         if ($position === false) {
